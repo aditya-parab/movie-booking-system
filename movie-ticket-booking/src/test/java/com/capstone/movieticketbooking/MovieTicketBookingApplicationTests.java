4cf99ef3381f996c399e24220f1d55329929d38a
aditya <adparab97+0@gmail.com>
2023-08-22 12:00:01 +0530

    first commit

---
+++ movie-ticket-booking/src/test/java/com/capstone/movieticketbooking/MovieTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.movieticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
